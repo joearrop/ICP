497ef0982c4e399b6bc96535b05797f6d615d8b6
Joe Ferreira Scholtz <joearrop@gmail.com>
2022-01-31 17:54:15 +0100

    pcd parser

--- src/main.cpp
+++ src/main.cpp
@@ -10,28 +10,11 @@
 #include <memory>
 #include <Eigen/Dense>
 #include "icp.hpp"
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
 int main(){
     std::cout << "Starting...\n";
-    /*
-    Eigen::MatrixXd matA(8,6);
-    matA << Eigen::MatrixXd::Identity(8,6);
-    Eigen::VectorXd vA(8,1);
-    vA << Eigen::MatrixXd::Ones(8,1);
-    Eigen::HouseholderQR<Eigen::Ref<Eigen::MatrixXd> > qr(matA);
-    std::cout << "qr.solve(vA): " << qr.solve(vA) << "\n";
-
-    Eigen::Matrix<double,3,1> vB;
-    vB << Eigen::MatrixXd::Ones(3,1);
-
-    auto a{vB.cross(vB)};
-    std::cout << "vB.cross(vB): " << a.transpose();
-    Eigen::Matrix<double,Eigen::Dynamic,3> mr(2,3);
-    for(int it=0,itMax=a.size();it<itMax;it++){
-        mr(1,it) = a[it];
-    }
-    std::cout << "\nmr: "<< mr;
-    */
     Eigen::MatrixXd matA(1000,3);
     matA << Eigen::MatrixXd::Random(1000,3);
     for(int it=0,itMax=(int)matA.rows();it<itMax;it++){
@@ -44,14 +27,26 @@ int main(){
         matB.row(it) += 0.2*Eigen::Matrix<double,1,3>::Ones();
     }
 
-//    auto a{dot(matA,matB)};
-//    auto b{dot(matA,matB)};
-//    std::cout<<"a: " << a<<"\n";
-//    std::cout<<"b: " << b<<"\n";
-
     ICP::Estimator estimator(matA,matB,ICP::nullTransform());
     std::cout << "Computing model...\n";
     auto Tf = estimator.computeModel();
     std::cout << "Tf:\n" << Tf << "\n";
+
+    /*
+
+    std::string fileName;
+    std::cout << "Enter point cloud name:\n";
+    std::cin >> fileName;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> ("data/"+fileName+".pcd", *cloud) == -1) //* load the file
+    {
+        PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+        return (-1);
+    }
+
+    */
+
+
     return 0;
 }
